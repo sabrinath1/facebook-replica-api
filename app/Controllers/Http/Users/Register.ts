4183404da18c445bc507e2c6c3edd9e1dc5f7509
Sabrina <sabrinath1@hotmail.com>
2022-11-03 16:38:32 -0300

    finalization user register

--- app/Controllers/Http/Users/Register.ts
+++ app/Controllers/Http/Users/Register.ts
@@ -1,29 +1,31 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { StoreValidator } from 'App/Validators/User/Register'
+import { StoreValidator, UpdateValidator } from 'App/Validators/User/Register'
 import { User, UserKey } from 'App/Models'
 import { faker } from '@faker-js/faker'
 import Mail from '@ioc:Adonis/Addons/Mail'
-import { UpdateValidator } from 'App/Validators/User/Register'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class UserRegistersController {
   public async store({ request }: HttpContextContract) {
-    const { email, redirectUrl } = await request.validate(StoreValidator)
+    await Database.transaction(async (trx) => {
+      const { email, redirectUrl } = await request.validate(StoreValidator)
+      const user = new User()
+      user.useTransaction(trx)
+      user.email = email
+      await user.save()
 
-    console.log(email, redirectUrl)
-    const user = await User.create({ email })
-    await user.save()
+      const key = faker.datatype.uuid() + new Date().getTime()
 
-    const key = faker.datatype.uuid() + new Date().getTime()
+      user.related('keys').create({ key })
 
-    user.related('keys').create({ key })
+      const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
 
-    const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
-
-    await Mail.send((message) => {
-      message.to(email),
-        message.from('contato@facebook.com', 'Facebook'),
-        message.subject('Criação de Conta'),
-        message.htmlView('emails/register', { link })
+      await Mail.send((message) => {
+        message.to(email),
+          message.from('contato@facebook.com', 'Facebook'),
+          message.subject('Criação de Conta'),
+          message.htmlView('emails/register', { link })
+      })
     })
   }
 
@@ -34,14 +36,15 @@ export default class UserRegistersController {
     return user
   }
 
-  public async update({ request }: HttpContextContract) {
+  public async update({ request, response }: HttpContextContract) {
     const { key, name, password } = await request.validate(UpdateValidator)
     const userKey = await UserKey.findByOrFail('key', key)
     const user = await userKey.related('user').query().firstOrFail()
-    const username = name.split('')[0].toLocaleLowerCase() + new Date().getTime()
+    const username = name.split(' ')[0].toLocaleLowerCase() + new Date().getTime()
 
     user.merge({ name, password, username })
     await user.save()
     await userKey.delete()
+    return response.ok({ message: 'Ok' })
   }
 }
